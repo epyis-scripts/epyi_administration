2ca77cf0989affae7a31192991efc66df0db6bec
Epyi <epyi.contact@gmail.com>
2023-07-09 16:05:55 +0200

    Add a menu title

--- client/menus/main.lua
+++ client/menus/main.lua
@@ -1,6 +1,7 @@
 ---main_showContentThisFrame → Function to show the main menu content
 ---@return void
 function main_showContentThisFrame(playerGroup)
+    RageUI.Separator("Vous êtes : " .. Config.Groups[playerGroup].Prefix .. GetPlayerName(PlayerId()))
     RageUI.ButtonWithStyle("Gestion de mon personnage", nil, {RightLabel = "→"}, Config.Groups[playerGroup].Access["submenu_personnal.access"], function(_, _, _)end)
     RageUI.ButtonWithStyle("Gestion des joueurs", nil, {RightLabel = "→"}, Config.Groups[playerGroup].Access["submenu_players.access"], function(_, _, _)end)
     RageUI.ButtonWithStyle("Gestion des véhicules", nil, {RightLabel = "→"}, Config.Groups[playerGroup].Access["submenu_vehicles.access"], function(_, _, _)end)
