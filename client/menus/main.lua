d199b80eff6f8a66245ff66ff71f6978660a076d
Epyi <epyi.contact@gmail.com>
2023-07-09 19:11:03 +0200

    Little cleaning

--- client/menus/main.lua
+++ client/menus/main.lua
@@ -6,15 +6,16 @@ function main_showContentThisFrame(playerGroup)
     end, function()
         isStaffModeEnabled = false
     end)
-    if isStaffModeEnabled then
-        RageUI.Separator("")
-        RageUI.Separator(TranslateCap("main_menu_title1") .. Config.Groups[playerGroup].Prefix .. GetPlayerName(PlayerId()))
-        RageUI.Separator(TranslateCap("main_menu_title2") .. Config.Groups[playerGroup].Color .. Config.Groups[playerGroup].Label)
-        RageUI.Separator("")
-        RageUI.ButtonWithStyle(TranslateCap("main_menu_access_personnal"), TranslateCap("main_menu_access_personnal_desc"), {RightLabel = "→"}, Config.Groups[playerGroup].Access["submenu_personnal.access"], function(_, _, _)end)
-        RageUI.ButtonWithStyle(TranslateCap("main_menu_access_players"), TranslateCap("main_menu_access_players_desc"), {RightLabel = "→"}, Config.Groups[playerGroup].Access["submenu_players.access"], function(_, _, _)end)
-        RageUI.ButtonWithStyle(TranslateCap("main_menu_access_vehicles"), TranslateCap("main_menu_access_vehicles_desc"), {RightLabel = "→"}, Config.Groups[playerGroup].Access["submenu_vehicles.access"], function(_, _, _)end)
-        RageUI.ButtonWithStyle(TranslateCap("main_menu_access_reports"), TranslateCap("main_menu_access_reports_desc"), {RightLabel = "→"}, Config.Groups[playerGroup].Access["submenu_reports.access"], function(_, _, _)end)
-        RageUI.ButtonWithStyle(TranslateCap("main_menu_access_server"), TranslateCap("main_menu_access_server_desc"), {RightLabel = "→"}, Config.Groups[playerGroup].Access["submenu_server.access"], function(_, _, _)end)
+    if not isStaffModeEnabled then
+        return
     end
+    RageUI.Separator("")
+    RageUI.Separator(TranslateCap("main_menu_title1") .. Config.Groups[playerGroup].Prefix .. GetPlayerName(PlayerId()))
+    RageUI.Separator(TranslateCap("main_menu_title2") .. Config.Groups[playerGroup].Color .. Config.Groups[playerGroup].Label)
+    RageUI.Separator("")
+    RageUI.ButtonWithStyle(TranslateCap("main_menu_access_personnal"), TranslateCap("main_menu_access_personnal_desc"), {RightLabel = "→"}, Config.Groups[playerGroup].Access["submenu_personnal.access"], function(_, _, _)end)
+    RageUI.ButtonWithStyle(TranslateCap("main_menu_access_players"), TranslateCap("main_menu_access_players_desc"), {RightLabel = "→"}, Config.Groups[playerGroup].Access["submenu_players.access"], function(_, _, _)end)
+    RageUI.ButtonWithStyle(TranslateCap("main_menu_access_vehicles"), TranslateCap("main_menu_access_vehicles_desc"), {RightLabel = "→"}, Config.Groups[playerGroup].Access["submenu_vehicles.access"], function(_, _, _)end)
+    RageUI.ButtonWithStyle(TranslateCap("main_menu_access_reports"), TranslateCap("main_menu_access_reports_desc"), {RightLabel = "→"}, Config.Groups[playerGroup].Access["submenu_reports.access"], function(_, _, _)end)
+    RageUI.ButtonWithStyle(TranslateCap("main_menu_access_server"), TranslateCap("main_menu_access_server_desc"), {RightLabel = "→"}, Config.Groups[playerGroup].Access["submenu_server.access"], function(_, _, _)end)
 end
