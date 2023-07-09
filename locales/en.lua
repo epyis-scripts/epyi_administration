a2ecc570c6cd0228ce56ec64dbac9616e7f4ef42
Epyi <epyi.contact@gmail.com>
2023-07-09 16:33:42 +0200

    Add descs + Missing locales too

--- locales/en.lua
+++ locales/en.lua
@@ -7,8 +7,13 @@ Locales["en"] = {
     ["main_menu_title1"] = "You are : ",
     ["main_menu_title2"] = "Your rank is : ",
     ["main_menu_access_personnal"] = "Management of ~r~my character",
+    ["main_menu_access_personnal_desc"] = "Access to your character management",
     ["main_menu_access_players"] = "Management of ~r~players",
+    ["main_menu_access_players_desc"] = "Access the management of players currently online on the server",
     ["main_menu_access_vehicles"] = "Management of ~r~vehicles",
+    ["main_menu_access_vehicles_desc"] = "Access to vehicle management",
     ["main_menu_access_reports"] = "Management of ~r~reports",
-    ["main_menu_access_server"] = "Management ot ~r~the server"
+    ["main_menu_access_reports_desc"] = "Access the management of player help reports",
+    ["main_menu_access_server"] = "Management ot ~r~the server",
+    ["main_menu_access_server_desc"] = "Access advanced server management"
 }
