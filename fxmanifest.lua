a7337a296dca0503ab274fe3c972ca08f9d303fe
Epyi <epyi.contact@gmail.com>
2023-07-11 17:59:32 +0200

    Formatting(fxmanifest.lua) - By epyidev

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,46 +1,46 @@
-fx_version "cerulean"
-game "gta5"
+fx_version("cerulean")
+game("gta5")
 
-name "epyi_administration"
-description "A RageUI Administration for ESX Legacy 1.7.5 or newer"
-author "Epyi (https://discord.gg/VyRPheG6Es)"
-version "1.0"
+name("epyi_administration")
+description("A RageUI Administration for ESX Legacy 1.7.5 or newer")
+author("Epyi (https://discord.gg/VyRPheG6Es)")
+version("1.0")
 
-shared_scripts {
-    "shared/locale.lua",
-    "locales/*.lua",
+shared_scripts({
+	"shared/locale.lua",
+	"locales/*.lua",
 
-    "@es_extended/imports.lua",
-    "config.lua"
-}
+	"@es_extended/imports.lua",
+	"config.lua",
+})
 
-client_scripts {
-    "RageUI/RMenu.lua",
-    "RageUI/menu/RageUI.lua",
-    "RageUI/menu/Menu.lua",
-    "RageUI/menu/MenuController.lua",
-    "RageUI/components/*.lua",
-    "RageUI/menu/elements/*.lua",
-    "RageUI/menu/items/*.lua",
-    "RageUI/menu/panels/*.lua",
-    "RageUI/menu/windows/*.lua",
-    "RageUI/menu/Function.lua",
+client_scripts({
+	"RageUI/RMenu.lua",
+	"RageUI/menu/RageUI.lua",
+	"RageUI/menu/Menu.lua",
+	"RageUI/menu/MenuController.lua",
+	"RageUI/components/*.lua",
+	"RageUI/menu/elements/*.lua",
+	"RageUI/menu/items/*.lua",
+	"RageUI/menu/panels/*.lua",
+	"RageUI/menu/windows/*.lua",
+	"RageUI/menu/Function.lua",
 
-    "client/menus_init.lua",
-    "client/keys.lua",
-    "client/menus/main.lua",
-    "client/menus/m_personnal.lua"
-}
+	"client/menus_init.lua",
+	"client/keys.lua",
+	"client/menus/main.lua",
+	"client/menus/m_personnal.lua",
+})
 
-server_scripts {
-    "@oxmysql/lib/MySQL.lua",
-    "server/init_sql.lua",
-    "server/init_permissions.lua",
-    "server/permissions.lua"
-}
+server_scripts({
+	"@oxmysql/lib/MySQL.lua",
+	"server/init_sql.lua",
+	"server/perms_init.lua",
+	"server/permissions.lua",
+})
 
-dependencies {
+dependencies({
 	"es_extended",
-    "oxmysql",
-    "esx_status"
-}
+	"oxmysql",
+	"esx_status",
+})
