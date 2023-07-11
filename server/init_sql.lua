fb1f91726e741c1a248b613cab2760049bfab5ef
Epyi <epyi.contact@gmail.com>
2023-07-11 19:12:38 +0200

    Fix(init_sql.lua) Add "if not exist"

--- server/init_sql.lua
+++ server/init_sql.lua
@@ -2,7 +2,7 @@
 -- init the sql part of the script, automatically create tables if not exists and add default admin
 Citizen.CreateThread(function()
 	MySQL.insert(
-		"CREATE TABLE `admin_data` (`id` int(11) NOT NULL AUTO_INCREMENT,`owner` varchar(60) NOT NULL,`type` varchar(99) DEFAULT NULL,`data` longtext DEFAULT NULL,PRIMARY KEY (`id`)) ENGINE=InnoDB;",
+		"CREATE TABLE IF NOT EXISTS `admin_data` (`id` int(11) NOT NULL AUTO_INCREMENT,`owner` varchar(60) NOT NULL,`type` varchar(99) DEFAULT NULL,`data` longtext DEFAULT NULL,PRIMARY KEY (`id`)) ENGINE=InnoDB;",
 		{}
 	)
 end)
