a091a31d65d44d8547c5517fc47925a41a445bbf
Epyi <epyi.contact@gmail.com>
2023-07-12 16:07:34 +0200

    Formatting(all) Reformat overall

--- client/menus/main.lua
+++ client/menus/main.lua
@@ -1,19 +1,57 @@
 ---main_showContentThisFrame → Function to show the main menu content
 ---@return void
 function main_showContentThisFrame(playerGroup)
-    RageUI.Checkbox(TranslateCap("main_menu_staffmode"), TranslateCap("main_menu_staffmode_desc"), isStaffModeEnabled, {}, function()end, function()
-        isStaffModeEnabled = true
-    end, function()
-        isStaffModeEnabled = false
-    end)
-    if not isStaffModeEnabled then
-        return
-    end
-    RageUI.Separator("")
-    RageUI.ButtonWithStyle(TranslateCap("main_menu_access_personnal"), TranslateCap("main_menu_access_personnal_desc"), {RightLabel = "→"}, Config.Groups[playerGroup].Access["submenu_personnal_access"], function(_, _, _)
-    end, RMenu:Get("epyi_administration", "main_personnal"))
-    RageUI.ButtonWithStyle(TranslateCap("main_menu_access_players"), TranslateCap("main_menu_access_players_desc"), {RightLabel = "→"}, Config.Groups[playerGroup].Access["submenu_players_access"], function(_, _, _)end)
-    RageUI.ButtonWithStyle(TranslateCap("main_menu_access_vehicles"), TranslateCap("main_menu_access_vehicles_desc"), {RightLabel = "→"}, Config.Groups[playerGroup].Access["submenu_vehicles_access"], function(_, _, _)end)
-    RageUI.ButtonWithStyle(TranslateCap("main_menu_access_reports"), TranslateCap("main_menu_access_reports_desc"), {RightLabel = "→"}, Config.Groups[playerGroup].Access["submenu_reports_access"], function(_, _, _)end)
-    RageUI.ButtonWithStyle(TranslateCap("main_menu_access_server"), TranslateCap("main_menu_access_server_desc"), {RightLabel = "→"}, Config.Groups[playerGroup].Access["submenu_server_access"], function(_, _, _)end)
+	RageUI.Checkbox(
+		TranslateCap("main_menu_staffmode"),
+		TranslateCap("main_menu_staffmode_desc"),
+		isStaffModeEnabled,
+		{},
+		function() end,
+		function()
+			isStaffModeEnabled = true
+		end,
+		function()
+			isStaffModeEnabled = false
+		end
+	)
+	if not isStaffModeEnabled then
+		return
+	end
+	RageUI.Separator("")
+	RageUI.ButtonWithStyle(
+		TranslateCap("main_menu_access_personnal"),
+		TranslateCap("main_menu_access_personnal_desc"),
+		{ RightLabel = "→" },
+		Config.Groups[playerGroup].Access["submenu_personnal_access"],
+		function(_, _, _) end,
+		RMenu:Get("epyi_administration", "main_personnal")
+	)
+	RageUI.ButtonWithStyle(
+		TranslateCap("main_menu_access_players"),
+		TranslateCap("main_menu_access_players_desc"),
+		{ RightLabel = "→" },
+		Config.Groups[playerGroup].Access["submenu_players_access"],
+		function(_, _, _) end
+	)
+	RageUI.ButtonWithStyle(
+		TranslateCap("main_menu_access_vehicles"),
+		TranslateCap("main_menu_access_vehicles_desc"),
+		{ RightLabel = "→" },
+		Config.Groups[playerGroup].Access["submenu_vehicles_access"],
+		function(_, _, _) end
+	)
+	RageUI.ButtonWithStyle(
+		TranslateCap("main_menu_access_reports"),
+		TranslateCap("main_menu_access_reports_desc"),
+		{ RightLabel = "→" },
+		Config.Groups[playerGroup].Access["submenu_reports_access"],
+		function(_, _, _) end
+	)
+	RageUI.ButtonWithStyle(
+		TranslateCap("main_menu_access_server"),
+		TranslateCap("main_menu_access_server_desc"),
+		{ RightLabel = "→" },
+		Config.Groups[playerGroup].Access["submenu_server_access"],
+		function(_, _, _) end
+	)
 end
--- shared/locale.lua
+++ shared/locale.lua
@@ -2,21 +2,25 @@ Locales = {}
 
 function Translate(str, ...)
 	if not str then
-		print(("[^1ERROR^7] Resource ^5%s^7 You did not specify a parameter for the Locale function or the value is nil!"):format(GetInvokingResource() or GetCurrentResourceName()))
-		return 'Given translate function parameter is nil!'
+		print(
+			("[^1ERROR^7] Resource ^5%s^7 You did not specify a parameter for the Locale function or the value is nil!"):format(
+				GetInvokingResource() or GetCurrentResourceName()
+			)
+		)
+		return "Given translate function parameter is nil!"
 	end
 	if Locales[Config.Locale] then
 		if Locales[Config.Locale][str] then
 			return string.format(Locales[Config.Locale][str], ...)
-		elseif Config.Locale ~= 'en' and Locales['en'] and Locales['en'][str] then
-			return string.format(Locales['en'][str], ...)
+		elseif Config.Locale ~= "en" and Locales["en"] and Locales["en"][str] then
+			return string.format(Locales["en"][str], ...)
 		else
-			return 'Translation [' .. Config.Locale .. '][' .. str .. '] does not exist'
+			return "Translation [" .. Config.Locale .. "][" .. str .. "] does not exist"
 		end
-	elseif Config.Locale ~= 'en' and Locales['en'] and Locales['en'][str] then
-		return string.format(Locales['en'][str], ...)
+	elseif Config.Locale ~= "en" and Locales["en"] and Locales["en"][str] then
+		return string.format(Locales["en"][str], ...)
 	else
-		return 'Locale [' .. Config.Locale .. '] does not exist'
+		return "Locale [" .. Config.Locale .. "] does not exist"
 	end
 end
 
