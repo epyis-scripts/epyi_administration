eafdf296efb3a4012030689e0f59b90009af407f
Epyi <epyi.contact@gmail.com>
2023-07-23 15:35:02 +0200

    Fix(import.sql) Fix sql for old mysql versions

--- import.sql
+++ import.sql
@@ -1,5 +1,6 @@
-INSERT INTO `datastore` (name, label, shared)
-SELECT 'epyi_admin_userdata', 'epyi_administration data', 0
-WHERE NOT EXISTS (
-    SELECT 1 FROM `datastore` WHERE name = 'epyi_admin_userdata'
+INSERT INTO `datastore`(name, label, shared)
+VALUES(
+    'epyi_admin_userdata',
+    'epyi_administration data',
+    0
 );
