f9ca73834e96240fbc9fd6cede3e48d37867f6ab
Epyi <epyi.contact@gmail.com>
2023-08-23 11:04:21 +0200

    fix(sv/players): Player not revive on server side

--- client/misc.lua
+++ client/misc.lua
@@ -2,26 +2,33 @@
 ---@param ped ped
 ---@return void
 function revivePed(ped)
-	local coords = GetEntityCoords(ped)
+	local _ped = ped
+	local coords = GetEntityCoords(_ped)
+	local heading = GetEntityHeading(_ped)
+	TriggerServerEvent("epyi_administration:setDeathStatus", false)
+
 	DoScreenFadeOut(800)
+
 	while not IsScreenFadedOut() do
 		Wait(50)
 	end
+
 	local formattedCoords = {
 		x = ESX.Math.Round(coords.x, 1),
 		y = ESX.Math.Round(coords.y, 1),
 		z = ESX.Math.Round(coords.z, 1),
 	}
-	SetEntityCoordsNoOffset(ped, formattedCoords.x, formattedCoords.y, formattedCoords.z, false, false, false)
+
+	SetEntityCoordsNoOffset(_ped, formattedCoords.x, formattedCoords.y, formattedCoords.z, false, false, false)
 	NetworkResurrectLocalPlayer(formattedCoords.x, formattedCoords.y, formattedCoords.z, heading, true, false)
-	SetPlayerInvincible(ped, false)
-	ClearPedBloodDamage(ped)
+	SetPlayerInvincible(_ped, false)
+	ClearPedBloodDamage(_ped)
 	TriggerEvent("esx_basicneeds:resetStatus")
 	TriggerServerEvent("esx:onPlayerSpawn")
 	TriggerEvent("esx:onPlayerSpawn")
-	TriggerEvent("playerSpawned")
+	TriggerEvent("playerSpawned") -- compatibility with old scripts, will be removed soon
 	ClearTimecycleModifier()
-	SetPedMotionBlur(ped, false)
+	SetPedMotionBlur(_ped, false)
 	ClearExtraTimecycleModifier()
 	DoScreenFadeIn(800)
 end
